1295792dfd37f64457ab914b93f641a0cb4def07
Wasiqul Islam <islam.wasiqul@gmail.com>
2024-07-12 17:33:22 +0600

    Time tracker initial version

---
+++ TimeTracker/MainUI.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TimeTracker
+{
+    partial class MainUI
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnStart = new Button();
+            btnStop = new Button();
+            txtDetails = new TextBox();
+            lblSummary = new Label();
+            btnReset = new Button();
+            btnOpenFolder = new Button();
+            lblQuery = new Label();
+            SuspendLayout();
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(46, 76);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(94, 29);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(155, 76);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(94, 29);
+            btnStop.TabIndex = 1;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // txtDetails
+            // 
+            txtDetails.Location = new Point(46, 176);
+            txtDetails.Multiline = true;
+            txtDetails.Name = "txtDetails";
+            txtDetails.ReadOnly = true;
+            txtDetails.Size = new Size(421, 348);
+            txtDetails.TabIndex = 2;
+            // 
+            // lblSummary
+            // 
+            lblSummary.AutoSize = true;
+            lblSummary.Location = new Point(51, 32);
+            lblSummary.Name = "lblSummary";
+            lblSummary.Size = new Size(71, 20);
+            lblSummary.TabIndex = 3;
+            lblSummary.Text = "Summary";
+            // 
+            // btnReset
+            // 
+            btnReset.Location = new Point(356, 76);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(111, 29);
+            btnReset.TabIndex = 4;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // btnOpenFolder
+            // 
+            btnOpenFolder.Location = new Point(356, 121);
+            btnOpenFolder.Name = "btnOpenFolder";
+            btnOpenFolder.Size = new Size(111, 29);
+            btnOpenFolder.TabIndex = 5;
+            btnOpenFolder.Text = "Open Folder";
+            btnOpenFolder.UseVisualStyleBackColor = true;
+            btnOpenFolder.Click += btnOpenFolder_Click;
+            // 
+            // lblQuery
+            // 
+            lblQuery.AutoSize = true;
+            lblQuery.Location = new Point(451, 32);
+            lblQuery.Name = "lblQuery";
+            lblQuery.Size = new Size(16, 20);
+            lblQuery.TabIndex = 6;
+            lblQuery.Text = "?";
+            lblQuery.Click += lblQuery_Click;
+            // 
+            // MainUI
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(518, 567);
+            Controls.Add(lblQuery);
+            Controls.Add(btnOpenFolder);
+            Controls.Add(btnReset);
+            Controls.Add(lblSummary);
+            Controls.Add(txtDetails);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Name = "MainUI";
+            Text = "Time Tracker";
+            FormClosed += MainUI_FormClosed;
+            Load += MainUI_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnStart;
+        private Button btnStop;
+        private TextBox txtDetails;
+        private Label lblSummary;
+        private Button btnReset;
+        private Button btnOpenFolder;
+        private Label lblQuery;
+    }
+}
---
+++ TimeTracker/MainUI.cs
@@ -0,0 +1,228 @@
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+//Development started by Wasiqul Islam on 12/07/2024
+
+namespace TimeTracker
+{
+    public partial class MainUI : Form
+    {
+
+        bool shouldClose = false;
+
+        private string fileName = "time.txt";
+
+        private string? _filePath = null;
+        public string FilePath
+        {
+            get
+            {
+                if (_filePath == null)
+                {
+                    string? appPath = Path.GetDirectoryName(Application.ExecutablePath);
+                    if (appPath == null)
+                    {
+                        throw new Exception("Could not get application parent directory.");
+                    }
+                    _filePath = Path.Combine(appPath, fileName);
+                }
+                return _filePath;
+            }
+        }
+
+        public MainUI()
+        {
+            InitializeComponent();
+        }
+
+        private void MainUI_Load(object sender, EventArgs e)
+        {
+            lblSummary.Text = "";
+            CalculateTime();
+            var task = new Task(UpdateTime);
+            task.Start();
+        }
+
+        public void UpdateTime()
+        {
+            while (true)
+            {
+                if (shouldClose)
+                {
+                    break;
+                }
+                CalculateTime();
+                Thread.Sleep(1000 * 60);
+                if (shouldClose)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void CalculateTime()
+        {
+
+            try
+            {
+
+                List<DateTime> times = ReadTime();
+
+                TimeSpan total = new TimeSpan();
+                if (times.Count % 2 == 0)
+                {
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = false;
+                }
+                else
+                {
+                    btnStart.Enabled = false;
+                    btnStop.Enabled = true;
+                }
+                for (int i = 0; i < times.Count; i += 2)
+                {
+                    DateTime endTime = DateTime.Now;
+                    if (i + 1 < times.Count)
+                    {
+                        endTime = times[i + 1];
+                    }
+                    TimeSpan span = endTime - times[i];
+                    total += span;
+                }
+                string summary = $"{total:hh}:{total:mm}";
+
+                string details = "";
+                foreach (DateTime time in times)
+                {
+                    details += time.ToString() + Environment.NewLine;
+                }
+
+                this.Invoke((MethodInvoker)delegate {
+                    lblSummary.Text = summary;
+                    txtDetails.Text = details;
+                });
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            WriteTime();
+            CalculateTime();
+        }
+
+        public void WriteTime()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(FilePath, true))
+                {
+                    sw.WriteLine(DateTime.Now.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void ClearTime()
+        {
+            try
+            {
+                if (File.Exists(FilePath))
+                {
+                    File.Delete(FilePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public List<DateTime> ReadTime()
+        {
+            List<DateTime> times = new List<DateTime>();
+            try
+            {
+                if (File.Exists(FilePath))
+                {
+                    using (StreamReader sr = new StreamReader(FilePath))
+                    {
+                        string? line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (DateTime.TryParse(line, out DateTime time))
+                            {
+                                times.Add(time);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return times;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            WriteTime();
+            CalculateTime();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            DialogResult result =
+                MessageBox.Show("Are you sure you want to reset the time?",
+                "Reset Time", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                ClearTime();
+                CalculateTime();
+            }
+        }
+
+        private void lblQuery_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"Click Start to start tracking time, 
+click Stop to stop tracking time, 
+click Reset to reset the time.
+- Developed by Wasiqul Islam
+Email: islam.wasiqul@gmail.com
+Last updated: 12/07/2024",
+                "Time Tracker",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            string? directory = Path.GetDirectoryName(FilePath);
+            try
+            {
+                if (directory != null && Directory.Exists(directory))
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", directory);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Could not open the folder {directory}. Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MainUI_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.shouldClose = true;
+        }
+
+    }
+}
